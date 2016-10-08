63d41325ad2af7544cee666d03b77e1b404ddf12
Vagner P Landskron <vagnerlands@gmail.com>
2016-10-08 20:59:07 +0300

    First upload
    Author: Kevin Jones
    https://fuzzystring.codeplex.com/

---
+++ FuzzyString/LevenshteinDistance.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyString
+{
+    public static partial class ComparisonMetrics
+    { 
+        public static int LevenshteinDistance(this string source, string target)
+        {
+            int n = source.Length;
+            int m = target.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // initialize the top and right of the table to 0, 1, 2, ...
+            for (int i = 0; i <= n; d[i, 0] = i++) ;
+            for (int j = 1; j <= m; d[0, j] = j++) ;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+                    int min1 = d[i - 1, j] + 1;
+                    int min2 = d[i, j - 1] + 1;
+                    int min3 = d[i - 1, j - 1] + cost;
+                    d[i, j] = Math.Min(Math.Min(min1, min2), min3);
+                }
+            }
+            return d[n, m];
+        
+        /*
+        if (source.Length == 0) { return target.Length; }
+        if (target.Length == 0) { return source.Length; }
+
+        int distance = 0;
+
+        if (source[source.Length - 1] == target[target.Length - 1]) { distance = 0; }
+        else { distance = 1; }
+
+        return Math.Min(Math.Min(LevenshteinDistance(source.Substring(0, source.Length - 1), target) + 1,
+                                 LevenshteinDistance(source, target.Substring(0, target.Length - 1))) + 1,
+                                 LevenshteinDistance(source.Substring(0, source.Length - 1), target.Substring(0, target.Length - 1)) + distance);
+    */
+    }
+
+        public static double NormalizedLevenshteinDistance(this string source, string target)
+        {
+            int unnormalizedLevenshteinDistance = source.LevenshteinDistance(target);
+
+            return unnormalizedLevenshteinDistance - source.LevenshteinDistanceLowerBounds(target);
+        }
+
+        public static int LevenshteinDistanceUpperBounds(this string source, string target)
+        {
+            // If the two strings are the same length then the Hamming Distance is the upper bounds of the Levenshtien Distance.
+            if (source.Length == target.Length) { return source.HammingDistance(target); }
+
+            // Otherwise, the upper bound is the length of the longer string.
+            else if (source.Length > target.Length) { return source.Length; }
+            else if (target.Length > source.Length) { return target.Length; }
+
+            return 9999;
+        }
+
+        public static int LevenshteinDistanceLowerBounds(this string source, string target)
+        {
+            // If the two strings are the same length then the lower bound is zero.
+            if (source.Length == target.Length) { return 0; }
+
+            // If the two strings are different lengths then the lower bounds is the difference in length.
+            else { return Math.Abs(source.Length - target.Length); }
+        }
+
+    }
+}
---
+++ FuzzyStringConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuzzyString;
+
+namespace FuzzyStringConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string kevin = "C:/Projects/_saldoGastos_Setembro";
+            string kevyn = "C:/Projects/saldo  - GASTOS  Setembro";
+
+            List<FuzzyStringComparisonOptions> options = new List<FuzzyStringComparisonOptions>();
+            options.Add(FuzzyStringComparisonOptions.UseJaccardDistance);
+            options.Add(FuzzyStringComparisonOptions.UseNormalizedLevenshteinDistance);
+            options.Add(FuzzyStringComparisonOptions.UseOverlapCoefficient);
+            options.Add(FuzzyStringComparisonOptions.UseLongestCommonSubsequence);
+            //options.Add(FuzzyStringComparisonOptions.CaseSensitive);
+
+            Console.WriteLine(kevin.ApproximatelyEquals(kevyn, FuzzyStringComparisonTolerance.Weak, options.ToArray()));
+            Console.WriteLine(kevin.ApproximatelyEquals(kevyn, FuzzyStringComparisonTolerance.Normal, options.ToArray()));
+            Console.WriteLine(kevin.ApproximatelyEquals(kevyn, FuzzyStringComparisonTolerance.Strong, options.ToArray()));
+
+            Console.ReadLine();
+        }
+    }
+}
